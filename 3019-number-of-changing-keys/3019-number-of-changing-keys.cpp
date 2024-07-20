16d0aba7c72953dbe0caae12121331909f34d404
GOUTHAMCHOWDARY <132541802+goutham-kommuri@users.noreply.github.com>
2024-07-20 18:08:07 +0530

    Time: 0 ms (100%), Space: 8.8 MB (5.81%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countKeyChanges(string str) {
+
+        char previous = str[0];
+
+        int countChanges = 0;
+
+        for( auto ele : str)
+        {
+            if( ele != previous && ele + 32 != previous && ele - 32 != previous)
+            {
+                 previous = ele;
+                 countChanges += 1;
+            }
+        }   
+
+        return countChanges;
+    }
+};
